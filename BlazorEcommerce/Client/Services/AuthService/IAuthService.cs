26982c8671524e6c6617036b0ec1c758d0b1f54b
pmnaredo <pmnaredo+github@gmail.com>
2022-12-08 22:10:22 +0100

    Login services added to the client (TODO logout)

--- BlazorEcommerce/Client/Services/AuthService/IAuthService.cs
+++ BlazorEcommerce/Client/Services/AuthService/IAuthService.cs
@@ -5,5 +5,6 @@ namespace BlazorEcommerce.Client.Services.AuthService
     public interface IAuthService
     {
         Task<ServiceResponse<int>> Register(UserRegister request);
+        Task<ServiceResponse<string>> Login(UserLogin request);
     }
 }
