2186627b61a1908d3f531ef74889c0ec22c63ca7
pmnaredo <pmnaredo+github@gmail.com>
2022-12-12 17:16:38 +0100

    Order and order items models added

--- BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -18,6 +18,9 @@ namespace BlazorEcommerce.Server.Data
             modelBuilder.Entity<ProductVariant>()
                 .HasKey(p => new { p.ProductId, p.ProductTypeId });
 
+            modelBuilder.Entity<OrderItem>()
+                .HasKey(oi => new { oi.OrderId, oi.ProductId, oi.ProductTypeId });
+
             modelBuilder.Entity<ProductType>().HasData(
                 new ProductType { Id = 1, Name = "Default" },
                 new ProductType { Id = 2, Name = "Paperback" },
@@ -279,5 +282,7 @@ namespace BlazorEcommerce.Server.Data
         public DbSet<ProductVariant> ProductVariants { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
